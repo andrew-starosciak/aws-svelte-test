9acc6d73b6991c0914d443191bc08603d0280a4a
Andrew Starosciak <andrew-starosciak>
2023-03-05 01:11:54 -0500

    init

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import { adapter } from 'sveltekit-adapter-aws';
+import preprocess from 'svelte-preprocess';
+
+export default {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      autoDeploy: true,
+    }),
+  },
+};
